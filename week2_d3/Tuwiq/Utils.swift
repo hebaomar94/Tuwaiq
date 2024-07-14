a706074db40f8d18b77b7973f5a9c16a33bafca9
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2024-07-14 20:08:21 +0300

    Add files via upload

---
+++ week2_d3/Tuwiq/Utils.swift
@@ -0,0 +1,28 @@
+//
+//  Utils.swift
+//  Tuwiq
+//
+//  Created by Heba Omar94 on 22/04/2024.
+//
+
+import Foundation
+
+class Utils {
+
+    static func readInt() -> Int {
+        return Int(readLine()!)!
+    }
+    
+    static func readFloat() -> Float {
+        return Float(readLine()!)!
+    }
+    
+    static func readDouble() -> Double {
+        return Double(readLine()!)!
+    }
+    
+    static func readString() -> String {
+        return readLine()!
+    }
+
+}
