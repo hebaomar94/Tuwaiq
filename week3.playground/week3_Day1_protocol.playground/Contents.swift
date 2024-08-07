ceb6035b4a6e7ef7bb66c4c1030dd65d431cca86
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2024-08-07 15:00:48 +0300

    Add files via upload

---
+++ week3.playground/week3_Day1_protocol.playground/Contents.swift
@@ -0,0 +1,113 @@
+import UIKit
+
+//===========week3=========Day 1
+//Day 1
+
+//Struct
+// The primary difference between a class and a struct is that a struct does not require an initializer. However, if you define an initializer manually, you must complete it. Another key distinction is that structs do not support inheritance, unlike classes.
+//
+//struct Person {
+//    //properties
+//    var name: String
+//    var age: Int
+//    //behaviors
+//    
+//    func move () {
+//        print ("\(name) in age \(age) is moving ")
+//    }
+//    
+//    
+//}
+//var p1 =  Person(name: "omar", age: 27)
+//print (p1.name)
+//p1.move()
+//
+//
+////=======================Pass By Value W Pass by Reference =====================
+//
+//struct PersonStr {
+//    var name: String = "unnamed"
+//}
+//
+//class PersonClass {
+//    var name: String = "unnamed"
+//}
+//
+//var p1PersonStr = PersonStr()
+//p1PersonStr.name = "heba"
+////print (p1PersonStr)
+//
+//var p2PersonStr = p1PersonStr
+//p2PersonStr.name = "omar"
+//print(p2PersonStr)
+//print (p1PersonStr)
+////here this is the pass by value thats mean when i make a copy i make Independent version , when i change in new version thats not effect in older one because its creat a ****** new place in Memory ******
+//
+////here class
+//var p1PersonClass = PersonClass()
+//p1PersonClass.name = "heba"
+////print (p1PersonStr)
+//
+//var p2PersonClass = p1PersonClass
+//p1PersonClass.name = "omar"
+//print(p2PersonClass.name)
+//print (p1PersonClass.name)
+//
+////here in class is the pass by Reference thats mean when i make a copy  its change in the original copy so the new version delete the old version 
+//================ Protoclos ====================
+
+protocol Flyable {
+    //incase of varible i need to use get (take a value) and set (give it a value )
+    var ableToFly: Bool {
+        get
+    }
+    
+    
+    
+    func fly()
+
+}
+
+// here struct not inherit this called as argument or  application not inherit
+
+struct Plane: Flyable {
+    var ableToFly: Bool
+    
+    func fly() {
+        print (" Able to fly ")
+    }
+}
+
+class Bird: Flyable {
+    var ableToFly: Bool
+    
+    func fly() {
+        print("able to fly")
+    }
+    
+    init(ableToFly: Bool) {
+        self.ableToFly = ableToFly
+    }
+}
+// Protocol support Mutiple Inhertance
+
+protocol Move {
+    
+    func move()
+}
+
+class A {
+    
+}
+
+class B {
+    
+}
+
+class C: B , Move {
+    func move() {
+        print ("moving")
+    }
+    
+    
+}
