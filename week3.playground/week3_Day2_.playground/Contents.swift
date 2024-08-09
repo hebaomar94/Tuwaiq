6b377986e73efb4a4731e680f719817f33963f7f
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2024-08-09 18:39:01 +0300

    Add files via upload

---
+++ week3.playground/week3_Day2_.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+enum Student: String{
+    case Name = "heba"
+    case Age = "22"
+    case degree = "99"
+}
+
+var studentDetails:Student = .Age
+studentDetails = .Age
+print (studentDetails.rawValue)
+var student2: Student = .Name
+student2 = .Name
+print(student2.rawValue)
+
+// example two
+
+enum WorkingDays {
+    case mon
+    case tue
+    case wed
+    case th
+    case friday
+}
+
+var currentDays = WorkingDays.mon
+ 
+if currentDays == .mon {
+    print("its monday baby")
+}else if currentDays == .tue {
+    print ("we are in trip ")
+}else {
+    print ("go to sleep baby ")
+}
+
+print (currentDays)
